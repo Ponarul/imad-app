3f6bf6da77540138caf5e278af479daecd76d248
Ponarul <30830089+Ponarul@users.noreply.github.com>
2017-09-03 10:58:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,71 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var ArticleOne = {
+    title: 'Article-One',
+    heading:'Article-One',
+    date: 'Sep 3,2017',
+    content: `
+            <p>
+                sdjvnjsekvo mawfmsmdvm
+                SDVSOAMV4SDV,L;SD,DVSDV SVDNnsesvnE
+            </p>
+            <p>
+                sdjvnjsekvo mawfmsmdvm
+                SDVSOAMV4SDV,L;SD,DVSDV SVDNnsesvnE
+            </p>
+            <p>
+                sdjvnjsekvo mawfmsmdvm
+                SDVSOAMV4SDV,L;SD,DVSDV SVDNnsesvnE
+            </p> `
+};
+
+function createtemplate (data) {
+var title=data.title   ;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+var htmltemplate =`
+    <html>
+    <head>
+        <title>
+           $(title)
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+        <hr/>
+        <h3>
+            $(heading)
+        </h3>
+        <div>
+            $(date)
+        </div>
+        <div>
+        $(content)
+        </div>
+        </div>
+        
+    </body>
+    
+    
+</html> `;
+return htmltemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+    res.send(createtemplate(articleOne));
 });
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
